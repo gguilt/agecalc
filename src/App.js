c84219439290324a169ca64240265aa9dbad5ea9
gguilt <58526116+gguilt@users.noreply.github.com>
2022-12-01 18:35:36 +0300

    Initialized React

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Age Calculator</h1>
+
+      <label>Name</label>
+      <input />
+      <label>Age (in years)</label>
+      <input />
+      <button>Add person</button>
+      <hr />
+    </div>
+  );
+}
+
+export default App;
